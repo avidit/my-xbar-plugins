854e0b32fb4bf74a722769446250c91b04569d01
avidit <aabishkar@gmail.com>
2022-02-08 10:59:32 -0500

    Use xbar variables

--- homeassistant/README.md
+++ homeassistant/README.md
@@ -2,6 +2,6 @@
 
 ![homeassistant](homeassistant.png)
 
-- Copy script to `~/Library/Application Support/xbar/plugins` and update the following:
+- Copy script to `~/Library/Application Support/xbar/plugins` and update the following variables in xbar plugins browser:
   - `HOMEASSISTANT_URL`
   - `HOMEASSISTANT_TOKEN`
--- homeassistant/homeassistant.5m.py
+++ homeassistant/homeassistant.5m.py
@@ -7,6 +7,10 @@
 # <xbar.image>https://raw.githubusercontent.com/avidit/my-xbar-plugins/develop/homeassistant/homeassistant.png</xbar.image>
 # <xbar.dependencies>python</xbar.dependencies>
 
+# Variables:
+# <xbar.var>string(HOMEASSISTANT_URL="http://homeassistant.local:8123/"): Home assistant URL.</xbar.var>
+# <xbar.var>string(HOMEASSISTANT_TOKEN="ey..."): Long Lived Access Token. See: https://www.home-assistant.io/docs/authentication/</xbar.var>
+
 # Installation:
 # 1. Copy this script to your xbar plugin folder
 # 2. Ensure the plugin file is executable by running chmod +x homeassistant.5m.py
@@ -15,11 +19,10 @@ import json
 import argparse
 from requests import get, post
 
-HOMEASSISTANT_URL = "http://homeassistant.local:8123/"
-HOMEASSISTANT_TOKEN = "ey..."
-
-api_url = f"{HOMEASSISTANT_URL}api/"
-headers = {"Authorization": f"Bearer {HOMEASSISTANT_TOKEN}", "content-type": "application/json"}
+ha_url = os.environ['HOMEASSISTANT_URL']
+ha_token = os.environ['HOMEASSISTANT_TOKEN']
+api_url = f"{ha_url}api/"
+headers = {"Authorization": f"Bearer {ha_token}", "content-type": "application/json"}
 ha_icon = "iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABmJLR0QAAAAAAAD5Q7t/AAAACXBIWXMAAABgAAAAYADwa0LPAAABL0lEQVQ4y8WUPS+DYRSGrwodlYhIqFWEnbBWrF34IQz9D34Cg48gpJtRRHTSxaYdDNhMTfyEy+A03r7efgVxJyfPk/Nx5z7n+YD/gJpTc4PkjgxANg7UgFrsf6RsXK37hQd18idk90H0HGb4hlOaIjtWp8NOwldXX9XGsGSqZfU8rGwnGoOQ1VNFFwmFl6nYW9f2u5C1sRyWhe8zTZG11DV1Qj3IINxXC+p65HaQtu/hDjAGvAOnwDxwB1QymqnweS/ngLOoyQO7WW1X1Wu1qG6pebWZUNgM33bk3KjVJEfWS9kEDoFVYAooAa2wUvhWgCNgo98pV1MDf1JnE/GZUJlEX4VJLAC3cQiFmN1Sr4JR+mMRKCb2PdH3txkWaYVXwEtGXivWvYzY42+L+lt8AFUJtvq22mzaAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIyLTAyLTA3VDE5OjUyOjEzKzAwOjAwOfvcFAAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMi0wMi0wN1QxOTo1MjoxMyswMDowMEimZKgAAAAASUVORK5CYII="
 
 print(f"|templateImage={ha_icon}")
@@ -129,4 +132,4 @@ if __name__ == "__main__":
                 pass
 
     print("---")
-    print(f"Open Home Assistant | href={HOMEASSISTANT_URL}")
+    print(f"Open Home Assistant | href={ha_url}")
